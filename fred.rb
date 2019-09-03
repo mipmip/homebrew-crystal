2645643c7af54f1d9440d7b0b80257229ad79ec4
Pim Snel <pim@lingewoud.nl>
2019-09-03 13:52:38 +0200

    Fred release 0.3.3

--- fred.rb
+++ fred.rb
@@ -1,9 +1,9 @@
 class Fred < Formula
   desc "Fred, a front matter editor"
-  version "0.3.2"
+  version "0.3.3"
   homepage "https://github.com/mipmip/fred"
   url "https://github.com/mipmip/fred/archive/v#{version}.tar.gz"
-  sha256 "d3effd2f8484c351a89281ad9ddcb795a3ba770d7f063e14e56f103814719af8"
+  sha256 "c2da57990543b7528189fee6a8288fd8b61fac0be4d1cd4299c44185d2e9b271"
   depends_on "crystal-lang"
 
   def install
