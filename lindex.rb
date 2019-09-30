c059ef6d554c1228e4b766451cccbbda4179d847
Pim Snel <pim@lingewoud.nl>
2019-09-30 21:49:35 +0200

    lindex release 0.1.6

--- lindex.rb
+++ lindex.rb
@@ -1,9 +1,9 @@
 class Lindex < Formula
   desc "lindex, a markdown document indexer"
-  version "0.1.5"
+  version "0.1.6"
   homepage "https://github.com/mipmip/lindex"
   url "https://github.com/mipmip/lindex/archive/v#{version}.tar.gz"
-  sha256 "da94b7726d561581990b2b06f8c557e82a561cfa0bbffdd1f2b87848fcd55981"
+  sha256 "9da595b132094b4404533addb556d30b96ff597724b2f4287f3de12e60371ce0"
   depends_on "crystal-lang"
 
   def install
